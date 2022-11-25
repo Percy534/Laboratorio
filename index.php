15358b00081a6782bb558fab4d267db580e81d1f
@percybm43 <percybm43@gmail.com>
2022-11-25 10:34:15 -0600

    pruebas

--- index.php
+++ index.php
@@ -108,16 +108,16 @@
     echo'<div class="card-group mt-5">
 
     <div class="card">
-        <a href="receta.php"><img src="./imgs/'.$recipe['recipe_image', 1].'" class="card-img-top"
+        <a href="receta.php"><img src="./imgs/'.$recipe['recipe_image'].'" class="card-img-top"
             alt="...">
         </a>
         <div class="card-body">
           <a href="#"><img src="../imgs/coronas/corona-oro.png" alt="bronce" width="30" height="30">
-            <span class="badge text-bg-secondary">'.$recipe["recipe_likes", 1].'</span>
+            <span class="badge text-bg-secondary">'.$recipe["recipe_likes"].'</span>
           </a>
-          <div class="card-title botones-verdes2">'.$recipe["recipe_name", 1].'</div>
-          <p class="card-text">'.$recipe["recipe_time", 1].'</p>
-          <div class="card-title dificultad">'.$recipe["id_recipe_level", 1].'</div>
+          <div class="card-title botones-verdes2">'.$recipe["recipe_name"].'</div>
+          <p class="card-text">'.$recipe["recipe_time"].'</p>
+          <div class="card-title dificultad">'.$recipe["id_recipe_level"].'</div>
         </div>
       </div>';
   }
